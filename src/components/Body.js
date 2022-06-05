3890e043eb51541ffb2c28ba37cf3a57f5f6ed1c
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2022-06-05 21:58:50 +0100

    Display the context-menu on left-click

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,8 +3,21 @@ import ContextMenu from "./ContextMenu";
 import "../styles/Body.css";
 
 function Body() {
+  const contextMenu = document.getElementById("context-menu");
+
+  function handleMouseDown(e) {
+    if (e.button === 0) {
+      const { clientX: mouseXPosition, clientY: mouseYPosition } = e;
+
+      contextMenu.style.left = `${mouseXPosition}px`;
+      contextMenu.style.top = `${mouseYPosition}px`;
+
+      contextMenu.classList.add("visible");
+    }
+  }
+
   return (
-    <main>
+    <main onMouseDown={handleMouseDown}>
       <ContextMenu />
       <DonutPileImage />
     </main>
