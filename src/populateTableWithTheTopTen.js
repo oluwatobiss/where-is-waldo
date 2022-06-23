d50abca5c50df25dd7b514949edb840622044f22
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2022-06-23 21:58:12 +0100

    Create populateTableWithTheTopTen.js file

---
+++ src/populateTableWithTheTopTen.js
@@ -0,0 +1,137 @@
+import uniqid from "uniqid";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase-config";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function populateTableWithTheTopTen() {
+  const leaderboardTableBody = document.getElementById(
+    "leaderboard-table-body"
+  );
+  const newTopTenPlayers = [];
+  const topTenPlayersCollectionRef = collection(db, "topTenPlayers");
+  const topTenPlayersCollectionQuery = query(
+    topTenPlayersCollectionRef,
+    orderBy("time"),
+    limit(10)
+  );
+  const currentTopTenPlayersDocs = await getDocs(topTenPlayersCollectionQuery);
+  currentTopTenPlayersDocs.forEach((document) => {
+    newTopTenPlayers.push(document.data());
+  });
+
+  const tableRowElements = newTopTenPlayers.map(createTableRowElements);
+  tableRowElements.forEach((i) => leaderboardTableBody.append(i));
+
+  function createTableRowElements(doc, ind) {
+    if (ind === 0) {
+      const tr = document.createElement("tr");
+      tr.setAttribute("key", uniqid());
+
+      const rankTD = document.createElement("td");
+      const positionSpan = document.createElement("span");
+      const medalSpan = document.createElement("span");
+
+      positionSpan.append("#1");
+      medalSpan.append("🥇🏆");
+      rankTD.append(positionSpan, medalSpan);
+
+      const nameTD = document.createElement("td");
+      nameTD.classList.add("name-td");
+      nameTD.append(doc.name);
+
+      const timeTD = document.createElement("td");
+      timeTD.append(doc.time);
+
+      const dateTD = document.createElement("td");
+      dateTD.append(doc.date);
+
+      tr.append(rankTD, nameTD, timeTD, dateTD);
+      return tr;
+    }
+
+    if (ind === 1) {
+      const tr = document.createElement("tr");
+      tr.setAttribute("key", uniqid());
+
+      const rankTD = document.createElement("td");
+      const positionSpan = document.createElement("span");
+      const medalSpan = document.createElement("span");
+
+      positionSpan.append("#2");
+      medalSpan.append("🥈");
+      rankTD.append(positionSpan, medalSpan);
+
+      const nameTD = document.createElement("td");
+      nameTD.classList.add("name-td");
+      nameTD.append(doc.name);
+
+      const timeTD = document.createElement("td");
+      timeTD.append(doc.time);
+
+      const dateTD = document.createElement("td");
+      dateTD.append(doc.date);
+
+      tr.append(rankTD, nameTD, timeTD, dateTD);
+      return tr;
+    }
+
+    if (ind === 2) {
+      const tr = document.createElement("tr");
+      tr.setAttribute("key", uniqid());
+
+      const rankTD = document.createElement("td");
+      const positionSpan = document.createElement("span");
+      const medalSpan = document.createElement("span");
+
+      positionSpan.append("#3");
+      medalSpan.append("🥉");
+      rankTD.append(positionSpan, medalSpan);
+
+      const nameTD = document.createElement("td");
+      nameTD.classList.add("name-td");
+      nameTD.append(doc.name);
+
+      const timeTD = document.createElement("td");
+      timeTD.append(doc.time);
+
+      const dateTD = document.createElement("td");
+      dateTD.append(doc.date);
+
+      tr.append(rankTD, nameTD, timeTD, dateTD);
+      return tr;
+    }
+
+    if (ind >= 3) {
+      const tr = document.createElement("tr");
+      tr.setAttribute("key", uniqid());
+
+      const rankTD = document.createElement("td");
+      rankTD.append(`#${ind + 1}`);
+
+      const nameTD = document.createElement("td");
+      nameTD.classList.add("name-td");
+      nameTD.append(doc.name);
+
+      const timeTD = document.createElement("td");
+      timeTD.append(doc.time);
+
+      const dateTD = document.createElement("td");
+      dateTD.append(doc.date);
+
+      tr.append(rankTD, nameTD, timeTD, dateTD);
+      return tr;
+    }
+  }
+}
+
+export default populateTableWithTheTopTen;
